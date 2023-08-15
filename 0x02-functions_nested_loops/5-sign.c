e25ea1bbee4007d803879edc148c485982d33145
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-15 07:49:18 -0700

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,37 +1,27 @@
-#include <unistd.h> /* Include this header for the write function.*/
-
 #include "main.h"
 
-/**
- * print_sign - Prints the sign of a number.
- * @n: The number to check.
- *
- * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative.
- */
-int print_sign(int n)
-{
-    if (n > 0)
-    {
-        _putchar('+');
-        return 1;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return 0;
-    }
-    else
-    {
-        _putchar('-');
-        return -1;
-    }
-}
 
-/* This is just a placeholder for the _putchar function. Make sure it's correctly implemented. */
-int _putchar(char c)
-{
-    /* Your implementation of _putchar goes here */
-    return write(1, &c, 1);
-}
-
+	/**
+	 * print_sign - prints the sign of a number
+	 * @n: the int to check
+	 * Return: 1 and prints + if n is greater than zero
+	 * 0 and prints 0 if n is zero
+	 * -1 and prints - if n is less than zero
+	 */
+	int print_sign(int n)
+	{
+		if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		} else if (n == 0)
+		{
+			_putchar(48);
+			return (0);
+		} else if (n < 0)
+		{
+			_putchar('-');
+		}
+			return (-1);
+	}
 

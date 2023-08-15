ebe4dc6967c982f2428d71300a21a1e41f9cb9d9
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-15 08:20:03 -0700

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,8 +6,8 @@
 	 * 0 and prints 0 if n is zero
 	 * -1 and prints - if n is less than zero
 	 */
-	int print_sign(int n)/*print_sign - prints the sign of a number*/
-	{
+int print_sign(int n)/*print_sign - prints the sign of a number*/ 
+{
 		if (n > 0)
 		{
 			_putchar('+');
@@ -21,5 +21,5 @@
 			_putchar('-');
 		}
 			return (-1);
-	
-	}
+
+}

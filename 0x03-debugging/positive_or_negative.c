0fa2d36f33e2a9211c0e84dc81503ca813ebc122
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-17 05:26:38 -0700

    c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,12 +3,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-void positive_or_negative(int i) 
-{
+void positive_or_negative(int i) {
     if (i > 0)
         printf("%d is positive\n", i);
     else if (i < 0)
         printf("%d is negative\n", i);
-    else
-        printf("%d is Zero\n");
+    else if (i == 0)
+        printf("%d is Zero\n", i);
 }

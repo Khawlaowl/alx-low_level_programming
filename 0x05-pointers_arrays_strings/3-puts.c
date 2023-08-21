1c607d38ea976c2598e67b5f73415b3dd15cad41
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-21 06:04:46 -0700

    c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string, followed by a new line, to the standard output (stdout).
+ * _puts - Prints a string, followed by a new line, to stdout.
  * @str: The string to be printed.
  */
 void _puts(char *str)

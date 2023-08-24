924dc9efb1e7752d742b6a6fbfbe9623d1df2f22
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-24 09:46:42 -0700

    c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,13 +8,7 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-  /*
-   * Corrected line of code:
-   * Assign the value 98 to the memory location pointed to by p + 5.
-   * This effectively modifies the value of a[2].
-   */
   *(p + 5) = 98;
-
   printf("a[2] = %d\n", a[2]);
   return (0);
 }

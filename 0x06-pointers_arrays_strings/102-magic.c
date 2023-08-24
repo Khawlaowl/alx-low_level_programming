9874141e2d5539864ac01cf1faeb3ba955342724
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-24 10:00:13 -0700

    c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description:
- * This program demonstrates how to modify the value of a[2] indirectly
- * using pointer arithmetic and print the modified value.
- *
- * Return: Always 0 (success)
- */
 int main(void)
 {
-    int n;
-    int a[5];
-    int *p;
-    a[2] = 1024;
-    p = &n;
+  int n;
+  int a[5];
+  int *p;
 
-    /*
-     * Write the value 98 to the memory location calculated by adding 5 integers (20 bytes) to the address
-     * pointed by p. This indirectly modifies the value of a[2].
-     */
-    *(p + 5) = 98;
-
-    printf("a[2] = %d\n", a[2]);
-
-    return (0);
+  a[2] = 1024;
+  p = &n;
+  *(p + 5) = 98;
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }

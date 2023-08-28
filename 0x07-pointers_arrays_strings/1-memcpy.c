a5228172e41f8c96e9194dd1e8bccb0e84085315
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-28 10:19:44 -0700

    c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies n bytes from memory area src to memory area dest.
+ * _memcpy - Copies n from memory  src to memory  dest.
  * @dest: Pointer to the destination memory area.
  * @src: Pointer to the source memory area.
  * @n: Number of bytes to copy.

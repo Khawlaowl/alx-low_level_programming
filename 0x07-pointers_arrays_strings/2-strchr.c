8504c999de51e5347a7353019c09d0ddb9b20501
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-28 10:22:55 -0700

    c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: The string to search in
  * @c: The character to locate
  *
- * Return: Pointer to the first occurrence of the character c in the string s,
+ * Return: Pointer  c in the string s,
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)

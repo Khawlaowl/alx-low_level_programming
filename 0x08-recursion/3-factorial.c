6b4d405631c4c945e0142971cf458b164e9c6b89
Khawlaowl <khawlahilli20045@gmail.com>
2023-08-30 09:37:30 -0700

    c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 /**
  * factorial - returns the factorial of a number
  * @n: The number for which factorial is calculated.
- * Description: This function calculates the  x raised to the power of y 
+ * Description: This function calculates the x raised to the power of y 
  *
  * Return: The factorial of n.
  */

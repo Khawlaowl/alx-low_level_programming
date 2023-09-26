fc060704b79bed40f12acfdce69a32cebce724e3
Khawlaowl <khawlahilli20045@gmail.com>
2023-09-26 02:57:17 -0700

    c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - fnct that deletes the head node and eturn the data
+ * @head: the head node
+ *
+ * Return: n
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *pntr;
+	int k;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	else
+	{
+		pntr = *head;
+		*head = (*head)->next;
+		k = pntr->n;
+		free(pntr);
+
+		return (k);
+	}
+}

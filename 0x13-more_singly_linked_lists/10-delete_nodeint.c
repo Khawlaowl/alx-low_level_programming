8449ffb37d5a22e05c6a8d2a7348494d325b09ce
Khawlaowl <khawlahilli20045@gmail.com>
2023-09-26 10:45:13 -0700

    c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,30 +13,30 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current4, *prev;
 	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+	return (-1);
 
-    current4 = *head;
+	current4 = *head;
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(current4);
-        return (1);
-    }
+	if (index == 0)
+	{
+	*head = (*head)->next;
+	free(current4);
+	return (1);
+	}
 
-    while (current4 != NULL)
-    {
-        if (i == index)
-        {
-            prev->next = current4->next;
-            free(current4);
-            return(1);
-        }
-        prev = current4;
-        current4 = current4->next;
-        i++;
-    }
+	while (current4 != NULL)
+	{
+	if (i == index)
+	{
+	prev->next = current4->next;
+	free(current4);
+	return (1);
+	}
+	prev = current4;
+	current4 = current4->next;
+	i++;
+	}
 
-    return (-1);
+	return (-1);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,37 +11,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 {
-	listint_t *n_node, c_node; 
+	listint_t *n_node, c_node;
 	unsigned int h = 0;
 
-
 	if (!head)
-		return(NULL);
+		return (NULL);
 	n_node = malloc(sizeof(unsigned int));
 	if (n_node == NULL)
 		return (NULL);
-	n_node->n =n;
+	n_node->n = n;
 
 	if (idx == 0)
-    {
-        n_node->next = *head;
-        *head = n_node;
-        return n_node;
-    }
+	{
+	n_node->next = *head;
+	*head = n_node;
+	return (n_node);
+	}
 
-    c_node= *head;
-    while (c_node != NULL)
-    {
-        if (h == idx - 1)
-        {
-            n_node->next = c_node->next;
-            c_node->next = n_node;
-            return n_node;
-        }
-        c-node = c_node->next;
-        h++;
-    }
+	c_node = *head;
+	while (c_node != NULL)
+	{
+	if (h == idx - 1)
+	{
+	n_node->next = c_node->next;
+	c_node->next = n_node;
+	return (n_node);
+	}
+	c_node = c_node->next;
+	h++;
+	}
 
-    free(n_node);
-    return NULL;
+	free(n_node);
+	return (NULL);
 }

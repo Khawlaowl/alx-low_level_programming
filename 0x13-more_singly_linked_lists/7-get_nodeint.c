ccb76c4f7ba8fb00156d48db9e9d7f2e024f2bd2
Khawlaowl <khawlahilli20045@gmail.com>
2023-09-26 03:25:35 -0700

    c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,9 +12,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int n= 0;
 	listint_t *temp = head;
 
-	if (head == NULL || *head == NULL)
-	return (NULL);
-
 	while (temp != NULL)
 	{
 		if (n == index)

2606f12d547e255ddabe6e9506ad131b902c35e4
Khawlaowl <khawlahilli20045@gmail.com>
2023-10-02 13:27:59 -0700

    c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,8 +13,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w;
-	long unsigned int len = 0;
+	int fd, w,  len = 0;
 
 	if (filename == NULL)
 		return (-1);

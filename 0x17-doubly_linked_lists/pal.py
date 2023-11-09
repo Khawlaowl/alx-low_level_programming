4bfcf70e212aec0b4ce29fe957dfeef0f3a1238a
Khawlaowl <khawlahilli20045@gmail.com>
2023-11-09 15:09:20 -0800

    r

--- 0x17-doubly_linked_lists/pal.py
+++ 0x17-doubly_linked_lists/pal.py
@@ -7,6 +7,8 @@ for x in range(100,1000):
     for y in range(100,1000):
         if(str(x*y) == str(x*y)[::-1]):
             palList.append(x*y)
-print(max(palList))
-with open('alx-low_level_programming/0x17-doubly_linked_lists/102-result', 'w') as file:
-    file.write(str(largest_palindrome))
+result = max(palList)
+
+# Save the result to the file with an absolute path
+with open('/alx-low_level_programming/0x17-doubly_linked_lists/102-result', 'w') as file:
+    file.write(str(result))
